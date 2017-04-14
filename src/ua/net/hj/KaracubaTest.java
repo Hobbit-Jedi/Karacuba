9ba0ef94cecd0c134d8fb18d02380a63e1219147
Hobbit Jedi <Hobbit_Jedi@mail.ru>
2017-04-15 00:07:14 +0300

    Создал новое хранилище.

---
+++ src/ua/net/hj/KaracubaTest.java
@@ -0,0 +1,358 @@
+package ua.net.hj;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Класс для тестирования Карацуба-чисел.
+ * @author Hobbit Jedi
+ */
+public class KaracubaTest {
+	public static HashMap<String, Integer> mSCounters = new HashMap<>();
+	
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		//testAdd();
+		//testSubstract();
+		//testMultiplicateKaracuba();
+		//testMultiplicateShumers();
+		contestKaracuba_vs_Shumers();
+	}
+	
+	/**
+	 * Выполнить тестирование операции сложения.
+	 */
+	public static void testAdd()
+	{
+		String[][] params;
+		
+		System.out.println("----------------------------");
+		System.out.println("| Add testing");
+		System.out.println("----------------------------");
+		params = new String[][]{
+									{"", "", "0"},
+									{"12d3234", "345", "Error"},
+									{"2", "3", "5"},
+									{"9", "8", "17"},
+									{"000", "000", "0"},
+									{"100", "2", "102"},
+									{"22", "1000", "1022"},
+									{"49823261", "44269423", "94092684"},
+									{"54761293", "65394884", "120156177"},
+									{"9313685456934674", "7658898761837539", "16972584218772213"}
+								};
+		for (String[] param : params)
+		{
+			System.out.println("----------------------------");
+			System.out.println("X = " + param[0]);
+			System.out.println("Y = " + param[1]);
+			try
+			{
+				NumberKaracuba X = new NumberKaracuba(new StringBuilder(param[0]));
+				NumberKaracuba Y = new NumberKaracuba(new StringBuilder(param[1]));
+				NumberKaracuba XY = X.add(Y);
+				System.out.println("X+Y = " + XY);
+				if (XY.toString().equals(param[2]))
+				{
+					System.out.println("OK");
+				}
+				else
+				{
+					System.out.println("!!! FAIL !!!");
+				}
+			}
+			catch (Exception ex)
+			{
+				System.out.println("Выброшено исключение: " + ex.getMessage());
+				if (param[2].equals("Error"))
+				{
+					System.out.println("OK");
+				}
+				else
+				{
+					System.out.println("!!! FAIL !!!");
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Выполнить тестирование операции вычитания.
+	 */
+	public static void testSubstract()
+	{
+		String[][] params;
+		
+		System.out.println("----------------------------");
+		System.out.println("| Substract testing");
+		System.out.println("----------------------------");
+		params = new String[][]{
+									{"", "", "0"},
+									{"12d3234", "345", "Error"},
+									{"2", "3", "Error"},
+									{"3", "2", "1"},
+									{"9", "8", "1"},
+									{"000", "000", "0"},
+									{"100", "2", "98"},
+									{"22", "1000", "Error"},
+									{"1000", "22", "978"},
+									{"49823261", "44269423", "5553838"},
+									{"54761293", "65394884", "Error"},
+									{"65394884", "54761293", "10633591"},
+									{"9313685456934674", "7658898761837539", "1654786695097135"}
+								};
+		for (String[] param : params)
+		{
+			System.out.println("----------------------------");
+			System.out.println("X = " + param[0]);
+			System.out.println("Y = " + param[1]);
+			try
+			{
+				NumberKaracuba X = new NumberKaracuba(new StringBuilder(param[0]));
+				NumberKaracuba Y = new NumberKaracuba(new StringBuilder(param[1]));
+				NumberKaracuba XY = X.substract(Y);
+				System.out.println("X-Y = " + XY);
+				if (XY.toString().equals(param[2]))
+				{
+					System.out.println("OK");
+				}
+				else
+				{
+					System.out.println("!!! FAIL !!!");
+				}
+			}
+			catch (Exception ex)
+			{
+				System.out.println("Выброшено исключение: " + ex.getMessage());
+				if (param[2].equals("Error"))
+				{
+					System.out.println("OK");
+				}
+				else
+				{
+					System.out.println("!!! FAIL !!!");
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Выполнить тестирование операции умножения (методом Карацубы).
+	 */
+	public static void testMultiplicateKaracuba()
+	{
+		String[][] params;
+		
+		System.out.println("----------------------------------");
+		System.out.println("| Multiplicate testing (Karacuba))");
+		System.out.println("----------------------------------");
+		params = new String[][]{
+								{"", "", "0"},
+								{"12d3234", "345", "Error"},
+								{"2", "3", "6"},
+								{"9", "8", "72"},
+								{"000", "000", "0"},
+								{"100", "2", "200"},
+								{"12345", "2", "24690"},
+								{"131", "70", "9170"},
+								{"1234", "5678", "7006652"},
+								{"49823261", "44269423", "2205647016448403", "8", "4", "7", "2"},
+								{"54761293", "65394884", "3581108403425012", "5", "2"},
+								{"9313685456934674", "7658898761837539", "71332574014261268360454523927286", "18", "1", "9", "3", "8", "7"},
+								{"3957322621234423", "7748313756335578", "30662577304368647842211393201494", "14", "1", "9", "2", "8", "8"},
+								{"34215432964249374812219364786397", "94541964835273822784327848699719", "3234794260129733170788831535430575611379062580407060392628922443", "45", "3", "9", "19", "8", "11"},
+								{"71611955325935479159397713213124", "93567726499788166681348352945366", "6700567850052179472481148730882040129649508491917840721086183384", "40", "4", "36", "2", "10", "4"},
+								{"8436939677358274975644341226884162349535836199962392872868456892", "3864264464372346883776335161325428226997417338413342945574123327", "32602566183268675582196165592691544162522778809155901895617284287276672563976841699892789718741377833554298336397153444191119684", "72", "4", "69", "3", "67", "1"},
+								{"8711129198194917883527844183686122989894424943636426448417394566", "2924825637132661199799711722273977411715641477832758942277358764", "25478534007255378799894857247961445544397925869179138904636157575535921570058983065006369481295619500406669960288667484926076424", "64", "5", "60", "3", "58", "5"},
+								{"21625695688898558125310188636840316594920403182768", "13306827740879180856696800391510469038934180115260", "287769407308846640970310151509826255482575362419155842891311909556878670000425352112987881085839680"},
+								{"1685287499328328297814655639278583667919355849391453456921116729", "7114192848577754587969744626558571536728983167954552999895348492", "11989460275519080564894036768322865785999566885539505969749975204962718118914971586072960191064507745920086993438529097266122668", "105", "3", "72", "7", "12", "5"}
+							};
+		for (String[] param : params)
+		{
+			System.out.println("----------------------------");
+			System.out.println("X = " + param[0]);
+			System.out.println("Y = " + param[1]);
+			try
+			{
+				NumberKaracuba X = new NumberKaracuba(new StringBuilder(param[0]));
+				NumberKaracuba Y = new NumberKaracuba(new StringBuilder(param[1]));
+				mSCounters.clear();
+				NumberKaracuba XY = X.multiplicate(Y);
+				System.out.println("X*Y = " + XY);
+				if (XY.toString().equals(param[2]))
+				{
+					System.out.println("OK");
+				}
+				else
+				{
+					System.out.println("!!! FAIL !!!");
+				}
+			}
+			catch (Exception ex)
+			{
+				System.out.println("Выброшено исключение: " + ex.getMessage());
+				if (param[2].equals("Error"))
+				{
+					System.out.println("OK");
+				}
+				else
+				{
+					System.out.println("!!! FAIL !!!");
+				}
+			}
+			System.out.print("{");
+			boolean isFirstEntry = true;
+			for (Map.Entry<String, Integer> entry: mSCounters.entrySet())
+			{
+				System.out.print((isFirstEntry?"":", ") + entry);
+				isFirstEntry = false;
+			}
+			System.out.println("}");
+			int i = 3;
+			while (i+1 < param.length)
+			{
+				String sWanted = param[i];
+				int sCounter;
+				if (KaracubaTest.mSCounters.containsKey(sWanted))
+				{
+					sCounter = mSCounters.get(sWanted);
+				}
+				else
+				{
+					sCounter = 0;
+				}
+				System.out.println("S = " + sWanted + " встречается " + sCounter + " раз.");
+				if (sCounter == Integer.valueOf(param[i+1]))
+				{
+					System.out.println("OK");
+				}
+				else
+				{
+					System.out.println("!!! FAIL !!!");
+				}
+				i += 2;
+			}
+		}
+	}
+	
+	/**
+	 * Выполнить тестирование операции умножения (методом Шумеров).
+	 */
+	public static void testMultiplicateShumers()
+	{
+		String[][] params;
+		
+		System.out.println("--------------------------------");
+		System.out.println("| Multiplicate testing (Shumers)");
+		System.out.println("--------------------------------");
+		params = new String[][]{
+								{"", "", "0"},
+								{"12d3234", "345", "Error"},
+								{"2", "3", "6"},
+								{"9", "8", "72"},
+								{"000", "000", "0"},
+								{"100", "2", "200"},
+								{"12345", "2", "24690"},
+								{"131", "70", "9170"},
+								{"1234", "5678", "7006652"},
+								{"49823261", "44269423", "2205647016448403"},
+								{"54761293", "65394884", "3581108403425012"},
+								{"9313685456934674", "7658898761837539", "71332574014261268360454523927286"},
+								{"3957322621234423", "7748313756335578", "30662577304368647842211393201494"},
+								{"34215432964249374812219364786397", "94541964835273822784327848699719", "3234794260129733170788831535430575611379062580407060392628922443"},
+								{"71611955325935479159397713213124", "93567726499788166681348352945366", "6700567850052179472481148730882040129649508491917840721086183384"},
+								{"8436939677358274975644341226884162349535836199962392872868456892", "3864264464372346883776335161325428226997417338413342945574123327", "32602566183268675582196165592691544162522778809155901895617284287276672563976841699892789718741377833554298336397153444191119684"},
+								{"8711129198194917883527844183686122989894424943636426448417394566", "2924825637132661199799711722273977411715641477832758942277358764", "25478534007255378799894857247961445544397925869179138904636157575535921570058983065006369481295619500406669960288667484926076424"},
+								{"21625695688898558125310188636840316594920403182768", "13306827740879180856696800391510469038934180115260", "287769407308846640970310151509826255482575362419155842891311909556878670000425352112987881085839680"},
+								{"1685287499328328297814655639278583667919355849391453456921116729", "7114192848577754587969744626558571536728983167954552999895348492", "11989460275519080564894036768322865785999566885539505969749975204962718118914971586072960191064507745920086993438529097266122668"}
+							};
+		for (String[] param : params)
+		{
+			System.out.println("----------------------------");
+			System.out.println("X = " + param[0]);
+			System.out.println("Y = " + param[1]);
+			try
+			{
+				NumberKaracuba X = new NumberKaracuba(new StringBuilder(param[0]));
+				NumberKaracuba Y = new NumberKaracuba(new StringBuilder(param[1]));
+				NumberKaracuba XY = X.multiplicateShumer(Y);
+				System.out.println("X*Y = " + XY);
+				if (XY.toString().equals(param[2]))
+				{
+					System.out.println("OK");
+				}
+				else
+				{
+					System.out.println("!!! FAIL !!!");
+				}
+			}
+			catch (Exception ex)
+			{
+				System.out.println("Выброшено исключение: " + ex.getMessage());
+				if (param[2].equals("Error"))
+				{
+					System.out.println("OK");
+				}
+				else
+				{
+					System.out.println("!!! FAIL !!!");
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Сравнить производительность умножения методом Шумеров и методом Карацубы.
+	 */
+	public static void contestKaracuba_vs_Shumers()
+	{
+		NumberKaracuba numX, numY, result;
+		long timeStart, timeFinish, timeDeltaKaracuba, timeDeltaShumers;
+		
+		int numLen = 10000;
+		
+		System.out.println("-----------------------");
+		System.out.println("| Karacuba vs. Shumers ");
+		System.out.println("-----------------------");
+		// Сгенерируем два охренительно больших числа для перемножения.
+		StringBuilder numXBuilder = new StringBuilder(numLen);
+		StringBuilder numYBuilder = new StringBuilder(numLen);
+		for (int i = 1; i <= numLen; i++)
+		{
+			numXBuilder.append((byte)(11*Math.random()));
+			numYBuilder.append((byte)(11*Math.random()));
+		}
+		System.out.println("X = " + numXBuilder);
+		System.out.println("Y = " + numYBuilder);
+		try
+		{
+			numX = new NumberKaracuba(numXBuilder);
+			numY = new NumberKaracuba(numYBuilder);
+			
+			// Карацуба
+			timeStart = System.nanoTime();
+			result = numX.multiplicate(numY);
+			timeFinish = System.nanoTime();
+			timeDeltaKaracuba = timeFinish - timeStart;
+			System.out.println("Karacuba X*Y = " + result);
+			System.out.println("" + timeDeltaKaracuba + " наносекунд.");
+			
+			// Шумеры
+			timeStart = System.nanoTime();
+			result = numX.multiplicateShumer(numY);
+			timeFinish = System.nanoTime();
+			timeDeltaShumers = timeFinish - timeStart;
+			System.out.println("Shumers X*Y = " + result);
+			System.out.println("" + timeDeltaShumers + " наносекунд.");
+			
+			System.out.println("Отношение Карацуба/Шумеры = " + (Double.valueOf(timeDeltaKaracuba)/Double.valueOf(timeDeltaShumers)) + " раз.");
+		}
+		catch (Exception e)
+		{
+			System.out.println("Вылетело исключение: " + e);
+		}
+		
+	}
+}
---
+++ src/ua/net/hj/NumberKaracuba.java
@@ -0,0 +1,267 @@
+package ua.net.hj;
+
+/**
+ * Представляет собой целое число, которое умеет умножаться
+ * на другое число так, как учил Великий Карацуба.
+ * @author Hobbit Jedi
+ */
+public class NumberKaracuba {
+	private final StringBuilder mNumber; // Строковое представление хранимого в объекте числа.
+	
+	/**
+	 * Сохранить большое число в объекте Карацубы.
+	 * @param aNumber - Строковое представление сохраняемого в объекте числа.
+	 * @throws java.lang.Exception - Если переданная строка не может быть преобразована в число,
+	 *                               то вызывает исключение.
+	 */
+	public NumberKaracuba(StringBuilder aNumber) throws Exception
+	{
+		int aNumberLen = aNumber.length();
+		mNumber = new StringBuilder(aNumberLen);
+		boolean isValuable = false;
+		for (int i = 0; i < aNumberLen; i++)
+		{
+			char currentChar = aNumber.charAt(i);
+			if (Character.isDigit(currentChar))
+			{
+				if (isValuable | currentChar != '0')
+				{
+					isValuable = true;
+					mNumber.append(currentChar);
+				}
+			}
+			else
+			{
+				throw new Exception("Неудачная попытка преобразовать строку в число.");
+			}
+		}
+		if (mNumber.length() == 0)
+		{
+			mNumber.append('0');
+		}
+	}
+	
+	/**
+	 * Получить длину хранимого числа.
+	 * @return - Длина хранимого числа (в десятичных разрядах).
+	 */
+	public int length()
+	{
+		return mNumber.length();
+	}
+	
+	/**
+	 * Получить строковое представление хранимого числа.
+	 * @return - Строковое представление хранимого числа.
+	 */
+	@Override
+	public String toString()
+	{
+		return mNumber.toString();
+	}
+	
+	/**
+	 * Прибавить к существующему числу другое Карацуба-число.
+	 * @param other - Карацуба-число, которое нужно прибавить к текущему числу.
+	 * @return - Новое Карацуба-число, представляющее собой результат сложения двух Карацуба-чисел.
+	 * @throws java.lang.Exception - В случае ошибок выбрасывает исключение.
+	 */
+	public NumberKaracuba add(NumberKaracuba other) throws Exception
+	{
+		int lenX = this.length();
+		int lenY = other.length();
+		int maxLen = Math.max(lenX, lenY);
+		StringBuilder resultBuilder = new StringBuilder(maxLen+1);
+		byte carry = 0;
+		for (int i = 0; i < maxLen; i++)
+		{
+			byte x = (i < lenX) ? (byte)(this.mNumber.charAt(lenX-1-i) - '0')  : 0;
+			byte y = (i < lenY) ? (byte)(other.mNumber.charAt(lenY-1-i) - '0') : 0;
+			byte add = (byte)(x + y + carry);
+			resultBuilder.insert(0, (char)('0' + (byte)(add % 10)));
+			carry = (byte)(add / 10);
+		}
+		if (carry != 0)
+		{
+			resultBuilder.insert(0, (char)('0' + carry));
+		}
+		try
+		{
+			return new NumberKaracuba(resultBuilder);
+		}
+		catch (Exception ex)
+		{
+			throw new Exception("Ошибка при сложении Карацуба-чисел: Результирующая строка получилась нечисловой.");
+		}
+	}
+	
+	/**
+	 * Отнять от существующего числа другое Карацуба-число.
+	 * @param other - Карацуба-число, которое нужно отнять от текущего числа.
+	 * @return - Новое Карацуба-число, представляющее собой результат вычитания указанного числа от текущего.
+	 * @throws java.lang.Exception - В случае ошибок выбрасывает исключение.
+	 */
+	public NumberKaracuba substract(NumberKaracuba other) throws Exception
+	{
+		int lenX = this.length();
+		int lenY = other.length();
+		if (lenX >= lenY)
+		{
+			StringBuilder resultBuilder = new StringBuilder(lenX);
+			byte carry = 0;
+			for (int i = 0; i < lenX; i++)
+			{
+				byte x = (byte)(this.mNumber.charAt(lenX-1-i) - '0');
+				byte y = (i < lenY) ? (byte)(other.mNumber.charAt(lenY-1-i) - '0') : 0;
+				byte sub = (byte)(x - y - carry);
+				if (sub < 0)
+				{
+					sub += 10;
+					carry = 1;
+				}
+				else
+				{
+					carry = 0;
+				}
+				resultBuilder.insert(0, (char)('0' + sub));
+			}
+			if (carry == 0)
+			{
+				try
+				{
+					return new NumberKaracuba(resultBuilder);
+				}
+				catch (Exception ex)
+				{
+					throw new Exception("Ошибка при вычитании Карацуба-чисел: Результирующая строка получилась нечисловой.");
+				}
+			}
+			else
+			{
+				throw new Exception("Попытка вычесть из меньшего числа большее.");
+			}
+		}
+		else
+		{
+			throw new Exception("Попытка вычесть из меньшего числа большее.");
+		}
+	}
+	
+	/**
+	 * Умножить существующее число на другое Карацуба-число (методом Карацубы).
+	 * @param other - Карацуба-число, на которое нужно умножить текущее число.
+	 * @return - Новое Карацуба-число, представляющее собой результат умножения двух Карацуба-чисел.
+	 * @throws java.lang.Exception - В случае ошибок выбрасывает исключение.
+	 */
+	public NumberKaracuba multiplicate(NumberKaracuba other) throws Exception
+	{
+		NumberKaracuba result;
+		int lenX = this.length();
+		int lenY = other.length();
+		int maxLen = Math.max(lenX, lenY);
+		if (maxLen == 1)
+		{
+			byte x = (byte)(this.mNumber.charAt(0) - '0');
+			byte y = (byte)(other.mNumber.charAt(0) - '0');
+			int factum = x * y;
+			StringBuilder resultBuilder = new StringBuilder(2);
+			resultBuilder.insert(0, (char)('0' + (byte)(factum % 10)));
+			byte carry = (byte)(factum / 10);
+			if (carry != 0)
+			{
+				resultBuilder.insert(0, (char)('0' + carry));
+			}
+			result = new NumberKaracuba(resultBuilder);
+		}
+		else
+		{
+			if ((maxLen & 1) == 1)
+			{
+				maxLen++;
+			}
+			int lenB = maxLen>>1;
+			int thisLenA = Math.max(this.length() - lenB, 0);
+			int otherLenA = Math.max(other.length() - lenB, 0);
+			NumberKaracuba xA = new NumberKaracuba(new StringBuilder(this.mNumber.substring(0, thisLenA)));
+			NumberKaracuba xB = new NumberKaracuba(new StringBuilder(this.mNumber.substring(thisLenA)));
+			NumberKaracuba yA = new NumberKaracuba(new StringBuilder(other.mNumber.substring(0, otherLenA)));
+			NumberKaracuba yB = new NumberKaracuba(new StringBuilder(other.mNumber.substring(otherLenA)));
+			NumberKaracuba xAyA = xA.multiplicate(yA);
+			NumberKaracuba xByB = xB.multiplicate(yB);
+			NumberKaracuba xAxBAdd = xA.add(xB);
+			NumberKaracuba yAyBAdd = yA.add(yB);
+			NumberKaracuba xAxBAdd_yAyBAdd = xAxBAdd.multiplicate(yAyBAdd);
+			NumberKaracuba S = xAxBAdd_yAyBAdd.substract(xAyA).substract(xByB);
+			//(DEBUG: Счетчик для задания 2
+			String numS = S.toString();
+			int currentCouner;
+			if (KaracubaTest.mSCounters.containsKey(numS))
+			{
+				currentCouner = KaracubaTest.mSCounters.get(numS);
+			}
+			else
+			{
+				currentCouner = 0;
+			}
+			KaracubaTest.mSCounters.put(numS, ++currentCouner);
+			//)DEBUG
+			
+			StringBuilder highOrdersBuilder = new StringBuilder(xAyA.length() + maxLen);
+			highOrdersBuilder.append(xAyA);
+			for (int i = 1; i <= maxLen; i++)
+			{
+				highOrdersBuilder.append('0');
+			}
+			NumberKaracuba highOrders = new NumberKaracuba(highOrdersBuilder);
+			
+			StringBuilder mediumOrdersBuilder = new StringBuilder(S.length() + lenB);
+			mediumOrdersBuilder.append(S);
+			for (int i = 1; i <= lenB; i++)
+			{
+				mediumOrdersBuilder.append('0');
+			}
+			NumberKaracuba mediumOrders = new NumberKaracuba(mediumOrdersBuilder);
+			
+			result = highOrders.add(mediumOrders).add(xByB);
+		}
+		return result;
+	}
+	
+	/**
+	 * Умножить существующее число на другое Карацуба-число (методом Шумеров).
+	 * @param other - Карацуба-число, на которое нужно умножить текущее число.
+	 * @return - Новое Карацуба-число, представляющее собой результат умножения двух Карацуба-чисел.
+	 * @throws java.lang.Exception - В случае ошибок выбрасывает исключение.
+	 */
+	public NumberKaracuba multiplicateShumer(NumberKaracuba other) throws Exception
+	{
+		NumberKaracuba result = new NumberKaracuba(new StringBuilder());
+		int lenX = this.length();
+		int lenY = other.length();
+		for (int i = lenY-1; i>=0; i--)
+		{
+			StringBuilder currentBuilder = new StringBuilder(lenX + 1);
+			byte carry = 0;
+			for (int j = lenX-1; j >= 0; j--)
+			{
+				byte x = (byte)(this.mNumber.charAt(j) - '0');
+				byte y = (byte)(other.mNumber.charAt(i) - '0');
+				int factum = x * y + carry;
+				currentBuilder.insert(0, (char)('0' + (byte)(factum % 10)));
+				carry = (byte)(factum / 10);
+			}
+			if (carry != 0)
+			{
+				currentBuilder.insert(0, (char)('0' + carry));
+			}
+			for (int k = 0; k < lenY - 1 - i; k++)
+			{
+				currentBuilder.append('0');
+			}
+			NumberKaracuba currentKaracuba = new NumberKaracuba(currentBuilder);
+			result = result.add(currentKaracuba);
+		}
+		return result;
+	}
+	
+}
